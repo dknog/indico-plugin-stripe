2a484694b94b9b6961f350b900e24eccab92096b
Allan Eising <allan@eising.dk>
2024-12-03 19:32:54 +0100

    Bump version

--- indico_payment_stripe/__init__.py
+++ indico_payment_stripe/__init__.py
@@ -12,7 +12,7 @@ Indico plugin for Stripe payment support.
 RELEASE = False
 
 
-__version_info__ = ("0", "1", "0")
+__version_info__ = ("0", "1", "1")
 __version__ = ".".join(__version_info__)
 __version__ += "-dev" if not RELEASE else ""
 
